0805e38e165fc7c3d56309611d18f1bba687f06b
c0decel <124975714+c0decel@users.noreply.github.com>
2023-07-19 01:03:34 -0700

    3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Crimson Peak",
-      director: "guillermo del toro",
-      genre: "romance",
-      description: "After marrying Thomas Sharpe (Tom Hiddleston), Edith Cushing (Mia Wasikowska) moves to his family''s decaying mansion, where she encounters supernatural entities and dark secrets",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/a/ad/Crimson_Peak_theatrical_poster.jpg"
-    },
-    {
-      id: 2,
-      title: "Silence of the Lambs",
-      director: "Jonathan Demme",
-      genre: "thriller",
-      description: "A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/1d661187b253755ef32e1f21748ca1e217529bb72f8ef8a87a18fd8e904246a0._RI_TTW_.jpg"
-    },
-    {
-      id: 3,
-      title: "The Dark Knight",
-      director: "christopher nolan",
-      genre: "dramatic",
-      description: "a superhero film and the second installment in Christopher Nolan''s Batman trilogy.",
-      image:
-        "https://m.media-amazon.com/images/I/91KkWf50SoL._AC_UF894,1000_QL80_.jpg"
-    }
-  ]);
-
+  const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://movie-apis-84b92f93a404.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.map((movie) => {
+          return {
+            Title: movie.Title,
+            Description: movie.Description,
+            Director: movie.Director.Name,
+            Bio: movie.Director.Bio,
+            Genre: movie.Genre.Name
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
@@ -52,7 +41,7 @@ export const MainView = () => {
     <div>
       {movies.map((movie) => (
         <MovieCard
-          key={movie.id}
+          key={movie.Title}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,23 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
-    );
-  };
-  
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.Title}
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired
+  }).isRequired,
+  director: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
